a8292188c375121452ec4d22bbc529d75e5850b2
blskye <echowht@qq.com>
2018-12-26 14:14:13 +0800

    pdo Oracle

--- sql_export/index.php
+++ sql_export/index.php
@@ -4,17 +4,28 @@
  * User: wanghaotian
  * Date: 2018/8/28
  * Time: 下午11:04
+ * 参考文章：https://blog.csdn.net/xiaodong_526/article/details/83860473
  */
-try {
-    $user="root";
-    $pass="";
-    $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
-    foreach($dbh->query('SELECT * FROM `my_log`;') as $row) {
-        print_r($row);
+$tns = "  
+(DESCRIPTION =
+    (ADDRESS_LIST =
+      (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
+    )
+    (CONNECT_DATA =
+      (SERVICE_NAME = orcl)
+    )
+  )
+       ";
+$db_username = "wz";
+$db_password = "ffffff";
+try{
+    $conn = new PDO("oci:dbname=".$tns,$db_username,$db_password);
+	foreach ($conn->query('SELECT count(1) from t_s_user') as $row) {
+        print_r($row); 
     }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+   
+    $conn = null;
+}catch(PDOException $e){
+    echo ($e->getMessage());
 }
 ?>
